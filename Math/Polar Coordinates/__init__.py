ce83ed8f0bc0fab8fbeddadbb16ffd34a55165a2
Prometheus731 <kakkaranshu731@gmail.com>
2022-02-28 01:09:16 +0530

    Polar Coordinate (complex numbers)

---
+++ Math/Polar Coordinates/__init__.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+
+complexNo = complex(input())
+print(type(complexNo))
+
+print(abs(complexNo))
+print(cmath.phase(complexNo))
